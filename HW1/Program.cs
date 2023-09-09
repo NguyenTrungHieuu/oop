1c72da507e3256f9ee2a1f6c4f26427c9035ce55
NguyenTrungHieuu <144231977+NguyenTrungHieuu@users.noreply.github.com>
2023-09-09 23:07:56 +0700

    Add files via upload

---
+++ HW1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Information
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nguyễn Trung Hiếu");
+            Console.WriteLine("Mã Sinh Viên 12522032");
+            Console.WriteLine("class 12422TN");
+            Console.WriteLine("username tài khoản github   NguyenTrungHieuu");
+            Console.WriteLine("nguyenueih12@gmail.com");
+        }
+    }
+}
